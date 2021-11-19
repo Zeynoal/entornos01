877e3e53821d3873085a14bcb6957e4013d890a8
Zeynoal <170072@iessierradeguara.com>
2021-11-19 11:07:07 +0100

    añadido proyecto de java

---
+++ holaMundo/src/holaMundo/holaMundo.java
@@ -0,0 +1,18 @@
+package holaMundo;
+
+public class holaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("La gente de bien ");
+		System.err.println("Los rojos");
+		while (1<2) {
+			tuMadre();
+		}
+
+	}
+	public static void tuMadre () {
+		System.out.println("Tu Madre");
+		
+	}
+
+}
